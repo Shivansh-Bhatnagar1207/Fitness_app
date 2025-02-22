588d07f2adec51baee46f7cb36157da0a37dffe6
Shivansh Bhatnagar <shivanshb55@gmail.com>
2025-02-23 05:16:18 +0530

    homepage UI update

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -6,21 +6,32 @@ import {
   ActivityIndicator,
   ImageBackground,
   TouchableOpacity,
+  Dimensions,
 } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useUser } from "@/context/UserContext";
+import Animated, {
+  useSharedValue,
+  useAnimatedScrollHandler,
+  interpolate,
+  Extrapolate,
+  Extrapolation,
+} from "react-native-reanimated";
+import { Ionicons } from "@expo/vector-icons";
+
+const { width } = Dimensions.get("window");
+const CARD_WIDTH = width * 0.9;
+const SLIDE_COUNT = 3;
 
 export default function Home() {
   const { userData, loading } = useUser();
-
   const { name, Height, Weight } = userData || {};
 
   let BMI = Weight / (Height * Height);
   BMI = BMI.toFixed(2);
-  let category;
-  let textColor;
 
+  let category, textColor;
   if (BMI < 18.5) {
     category = "Underweight";
     textColor = "text-red-500";
@@ -51,18 +62,26 @@ export default function Home() {
     );
   }
 
+  // Smooth Animation for Slider & Indicators
+  const scrollX = useSharedValue(0);
+  const scrollHandler = useAnimatedScrollHandler((event) => {
+    scrollX.value = event.contentOffset.x;
+  });
+
   return (
     <SafeAreaView className="bg-bgnd h-full">
       <ScrollView>
-        <View className=" ">
+        <View>
+          {/* Logo */}
           <Image
             source={require("../../assets/images/logo.png")}
             className="w-full h-[84px] mt-5"
             resizeMode="cover"
           />
 
+          {/* BMI Section */}
           <View className="py-5 bg-white w-[95vw]  my-10 mx-auto px-5  rounded-3xl">
-            {/* <Text className="text-4xl text-center font-bold py-2">BMI</Text> */}
+            {/* <Text className="text-4xl text-center font-semibold text-seconday py-2">BMI</Text> */}
             <View className="flex-row justify-between">
               <Text className="text-base p-2">
                 <Text className="font-semibold">Height : </Text>
@@ -107,105 +126,181 @@ export default function Home() {
               </View>
             </View>
           </View>
-
+          {/* Discover Section with Smooth Slider & Indicators */}
           <View>
-            <Text className=" text-center text-3xl font-bold text-accent">
+            <Text className="text-center text-3xl font-bold text-accent">
               Discover
             </Text>
 
-            <View>
-              <ScrollView horizontal>
-                <View className="flex-row gap-2 mx-auto">
-                  <View className="w-[90vw] h-24 bg-white mx-auto mt-10 rounded-3xl">
-                    <ImageBackground
-                      source={require("../../assets/images/confetti.png")}
-                      height={200}
-                      width={200}
-                      resizeMode="cover"
-                      className="flex-1"
-                    >
-                      <View className="flex justify-center h-24 px-6 gap-2">
-                        <Text className="text-xl font-bold ">
-                          "Big Goals start with small habits"
-                        </Text>
-                        <TouchableOpacity
-                          activeOpacity={0.7}
-                          className="bg-secondary h-8 w-28 rounded-full"
-                        >
-                          <Text className="my-auto text-white text-lg text-center font-semibold">
-                            Track Now
-                          </Text>
-                        </TouchableOpacity>
-                      </View>
-                    </ImageBackground>
-                  </View>
-                  <View className="w-[90vw] h-24 bg-white mx-auto mt-10 rounded-3xl">
-                    <ImageBackground
-                      source={require("../../assets/images/confetti.png")}
-                      height={200}
-                      width={200}
-                      resizeMode="cover"
-                      className="flex-1"
-                    >
-                      <View className="flex justify-center h-24 px-6 gap-2">
-                        <Text className="text-xl font-bold ">
-                          "Big Goals start with small habits"
-                        </Text>
-                        <TouchableOpacity
-                          activeOpacity={0.7}
-                          className="bg-secondary h-8 w-28 rounded-full"
-                        >
-                          <Text className="my-auto text-white text-lg text-center font-semibold">
-                            Track Now
-                          </Text>
-                        </TouchableOpacity>
-                      </View>
-                    </ImageBackground>
-                  </View>
-                  <View className="w-[90vw] h-24 bg-white mx-auto mt-10 rounded-3xl">
-                    <ImageBackground
-                      source={require("../../assets/images/confetti.png")}
-                      height={200}
-                      width={200}
-                      resizeMode="cover"
-                      className="flex-1"
-                    >
-                      <View className="flex justify-center h-24 px-6 gap-2">
-                        <Text className="text-xl font-bold ">
-                          "Big Goals start with small habits"
-                        </Text>
-                        <TouchableOpacity
-                          activeOpacity={0.7}
-                          className="bg-secondary h-8 w-28 rounded-full"
-                        >
-                          <Text className="my-auto text-white text-lg text-center font-semibold">
-                            Track Now
-                          </Text>
-                        </TouchableOpacity>
-                      </View>
-                    </ImageBackground>
-                  </View>
-                </View>
-              </ScrollView>
-              <View className="flex-row mx-auto gap-4">
-                <View className="w-[42vw] h-24 bg-white mt-10 rounded-3xl"></View>
-                <View className="w-[42vw] h-24 bg-white mt-10 rounded-3xl"></View>
+            <Animated.ScrollView
+              horizontal
+              pagingEnabled
+              showsHorizontalScrollIndicator={false}
+              onScroll={scrollHandler}
+              scrollEventThrottle={16}
+              className="mt-5"
+            >
+              {/* Slide 1 */}
+              <View
+                style={{
+                  width: CARD_WIDTH,
+                  marginHorizontal: 10,
+                  borderRadius: 20,
+                  overflow: "hidden",
+                  backgroundColor: "white",
+                }}
+              >
+                <ImageBackground
+                  source={require("../../assets/images/confetti3.jpg")}
+                  className="flex-1 justify-center px-5 h-32 w-full"
+                  resizeMode="cover"
+                >
+                  <Text className="text-xl text-white font-bold">
+                    Small changes, big results!
+                  </Text>
+                  <TouchableOpacity
+                    activeOpacity={0.7}
+                    className="bg-secondary h-8 w-28 rounded-full mt-2"
+                  >
+                    <Text className="text-white text-lg text-center font-semibold">
+                      Track Now
+                    </Text>
+                  </TouchableOpacity>
+                </ImageBackground>
+              </View>
+
+              {/* Slide 2 */}
+              <View
+                style={{
+                  width: CARD_WIDTH,
+                  marginHorizontal: 10,
+                  borderRadius: 20,
+                  overflow: "hidden",
+                  backgroundColor: "white",
+                }}
+              >
+                <ImageBackground
+                  source={require("../../assets/images/confetti3.jpg")}
+                  className="flex-1 justify-center px-5"
+                  resizeMode="cover"
+                >
+                  <Text className="text-xl text-white font-bold">
+                    Monitor your cycle with ease
+                  </Text>
+                  <TouchableOpacity
+                    activeOpacity={0.7}
+                    className="bg-secondary h-8 w-28 rounded-full mt-2"
+                  >
+                    <Text className="text-white text-lg text-center font-semibold">
+                      Join Workout
+                    </Text>
+                  </TouchableOpacity>
+                </ImageBackground>
+              </View>
+
+              {/* Slide 3 */}
+              <View
+                style={{
+                  width: CARD_WIDTH,
+                  marginHorizontal: 10,
+                  borderRadius: 20,
+                  overflow: "hidden",
+                  backgroundColor: "white",
+                }}
+              >
+                <ImageBackground
+                  source={require("../../assets/images/confetti3.jpg")}
+                  className="flex-1 justify-center px-5"
+                  resizeMode="cover"
+                >
+                  <Text className="text-xl text-white font-bold">
+                    Track of your daily calorie intake
+                  </Text>
+                  <TouchableOpacity
+                    activeOpacity={0.7}
+                    className="bg-secondary h-8 w-28 rounded-full mt-2"
+                  >
+                    <Text className="text-white text-lg text-center font-semibold">
+                      Start Now
+                    </Text>
+                  </TouchableOpacity>
+                </ImageBackground>
+              </View>
+            </Animated.ScrollView>
+
+            {/* Smooth Slider Indicators */}
+            <View className="flex-row justify-center mt-3">
+              {[...Array(SLIDE_COUNT)].map((_, index) => {
+                const animatedStyle = {
+                  transform: [
+                    {
+                      scale: interpolate(
+                        scrollX.value,
+                        [
+                          (index - 1) * CARD_WIDTH,
+                          index * CARD_WIDTH,
+                          (index + 1) * CARD_WIDTH,
+                        ],
+                        [0.8, 1.3, 0.8],
+                        Extrapolation.CLAMP
+                      ),
+                    },
+                  ],
+                  opacity: interpolate(
+                    scrollX.value,
+                    [
+                      (index - 1) * CARD_WIDTH,
+                      index * CARD_WIDTH,
+                      (index + 1) * CARD_WIDTH,
+                    ],
+                    [0.3, 1, 0.3],
+                    Extrapolate.CLAMP
+                  ),
+                  backgroundColor: "#FFFFFF",
+                  marginHorizontal: 5,
+                  borderRadius: 6,
+                  width: 10,
+                  height: 10,
+                };
+
+                return <Animated.View key={index} style={animatedStyle} />;
+              })}
+            </View>
+          </View>
+          <View className="flex-row mx-auto mt-2 gap-4">
+            <View className="flex justify-evenly">
+              <View className="flex items-center justify-center w-44 h-24 bg-white rounded-3xl border-primary border">
+                <Text className="font-semibold">
+                  Steps{" "}
+                  <Ionicons name="footsteps" size={20} color={"#FFBD73"} />
+                </Text>
+              </View>
+              <View className="flex items-center justify-center w-44 h-24 bg-white rounded-3xl border-primary border">
+                <Text className="font-semibold">
+                  Hydration{" "}
+                  <Ionicons name="water-outline" size={20} color={"#FFBD73"} />{" "}
+                </Text>
               </View>
             </View>
-            <View className=" flex-row py-10 gap-1 p-3">
-              <View className="bg-white h-20  w-[28vw]   mx-auto px-5 flex items-center justify-center rounded-full">
-                <Text className="font-bold text-lg ">0.00</Text>
-                <Text className=" text-l">KCAL</Text>
-              </View>
-              <View className="bg-white h-20 w-[28vw]  mx-auto px-5 flex items-center justify-center rounded-full">
-                <Text className="font-bold text-lg ">0</Text>
-                <Text className=" text-l">WORKOUTS</Text>
-              </View>
-              <View className="bg-white h-20 w-[28vw]  mx-auto px-5 flex items-center justify-center rounded-full">
-                <Text className="font-bold text-lg ">0</Text>
-                <Text className=" text-l">MINUTES</Text>
-              </View>
-            </View>
+            <ImageBackground
+              source={require("../../assets/images/hero.jpg")}
+              className="w-56 h-64 bg-white rounded-3xl overflow-hidden border border-primary"
+              resizeMode="cover"
+            ></ImageBackground>
+          </View>
+        </View>
+        <View className=" flex-row py-10 gap-1 p-3">
+          <View className="bg-white h-20  w-[28vw]   mx-auto px-5 flex items-center justify-center rounded-full">
+            <Text className="font-bold text-lg ">0.00</Text>
+            <Text className=" text-l">KCAL</Text>
+          </View>
+          <View className="bg-white h-20 w-[28vw]  mx-auto px-5 flex items-center justify-center rounded-full">
+            <Text className="font-bold text-lg ">0</Text>
+            <Text className=" text-l">WORKOUTS</Text>
+          </View>
+          <View className="bg-white h-20 w-[28vw]  mx-auto px-5 flex items-center justify-center rounded-full">
+            <Text className="font-bold text-lg ">0</Text>
+            <Text className=" text-l">MINUTES</Text>
           </View>
         </View>
       </ScrollView>
